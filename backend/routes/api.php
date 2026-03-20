30dc29bf48c8628f1b59f565ba4d15c17d7128b1
GabrielChaconA <citroustax8849@gmail.com>
2026-03-20 08:57:22 -0600

    nosubir

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgotPasswordController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/v1/auth/forgot-password', [ForgotPasswordController::class , 'sendResetLinkEmail']);
