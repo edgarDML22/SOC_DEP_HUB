b289b56516431c8a4dceeb3db51997e5a68d87ff
IsraelRamirezMorales <israelramirezp1@gmail.com>
2026-04-06 18:27:52 -0600

    First commit, daily work scheduler intregrated in docker-compose.yml

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('app:calcular-no-shows')
+    ->dailyAt('23:00')
+    ->timezone('America/Mexico_City');
