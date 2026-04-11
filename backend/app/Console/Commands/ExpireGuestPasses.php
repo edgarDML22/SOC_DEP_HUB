cbcea88d7f7563bedd2185797152b9fb24dc917c
IsraelRamirezMorales <israelramirezp1@gmail.com>
2026-04-11 02:16:50 -0600

    feat: [SDH-2901] add cron job for automatic guest pass expiration

---
+++ backend/app/Console/Commands/ExpireGuestPasses.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\PasesDiarios;
+use Carbon\Carbon;
+
+class ExpireGuestPasses extends Command
+{
+    // El nombre con el que ejecutarás el comando
+    protected $signature = 'passes:expire';
+
+    // Descripción para cuando listes los comandos
+    protected $description = 'Expira los pases de invitado activos al final del día';
+
+    public function handle()
+    {
+        $ahora = Carbon::now('America/Mexico_City')->format('Y-m-d');
+
+        $affected = PasesDiarios::where('estatus_acceso', 'ACTIVO')
+            ->where('fecha_activacion', '<=', $ahora)
+            ->update(['estatus_acceso' => 'EXPIRADO']);
+
+        $this->info("Proceso terminado. {$affected} pases han sido marcados como 'EXPIRADO'.");
+    }
+}
--- backend/routes/console.php
+++ backend/routes/console.php
@@ -9,6 +9,9 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-
 // Le decimos que corra tu comando de limpieza cada minuto
 Schedule::command('app:clean-expired-reservations')->everyMinute();
+
+Schedule::command('passes:expire')
+    ->dailyAt('23:59')
+    ->timezone('America/Mexico_City');
